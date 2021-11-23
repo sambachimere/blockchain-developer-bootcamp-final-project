d57613657fd2caecf683485d506ba20bd8d0d0c9
Samba Diaw <sambachimerediaw@gmail.com>
2021-11-23 15:46:23 +0100

    contracts & test done

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -9,22 +9,21 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "hardhat/console.sol";
 
 contract NFT is ERC721URIStorage {
-        using Counters for Counters.Counter;
-        Counters.Counter private _tokenIds;
-        address contractAddress;
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    address contractAddress;
 
-        constructor(address marketplaceAddress) ERC721("Metaverse Tokens", "METT") {
-                contractAddress = marketplaceAddress;
-        }
-        
-        function createToken(string memory tokenURI) public returns (uint) {
-                _tokenIds.increment();
-                uint256 newItemId = _tokenIds.current();
+    constructor(address marketplaceAddress) ERC721("Metaverse Tokens", "METT") {
+        contractAddress = marketplaceAddress;
+    }
 
-                _mint(msg.sender, newItemId);
-                _setTokenURI(newItemId, tokenURI);
-                setApprovalForAll(contractAddress, true);
-                return newItemId;
-        }
+    function createToken(string memory tokenURI) public returns (uint) {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        setApprovalForAll(contractAddress, true);
+        return newItemId;
+    }
 }
-
